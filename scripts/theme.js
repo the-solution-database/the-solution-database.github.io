7b8b58765113c8fbdea87b05c6d669d4dac92252
wendellmeset <mattinsh@yahoo.com>
2025-04-16 08:08:32 -0700

    Make default theme dark

--- scripts/theme.js
+++ scripts/theme.js
@@ -1,20 +1,23 @@
 document.addEventListener('DOMContentLoaded', () => {
     const themeToggle = document.querySelector('#checkbox');
-    
-    // Check for saved theme preference
-    if (localStorage.getItem('theme') === 'dark') {
+
+    // dark theme default
+    if (localStorage.getItem('theme') !== 'light') {
         document.body.classList.add('dark-theme');
+        themeToggle.checked = false;
+    } else {
+        document.body.classList.remove('dark-theme');
         themeToggle.checked = true;
     }
-    
+
     // Handle theme toggle
-    themeToggle.addEventListener('change', function() {
+    themeToggle.addEventListener('change', function () {
         if (this.checked) {
-            document.body.classList.add('dark-theme');
-            localStorage.setItem('theme', 'dark');
-        } else {
             document.body.classList.remove('dark-theme');
             localStorage.setItem('theme', 'light');
+        } else {
+            document.body.classList.add('dark-theme');
+            localStorage.setItem('theme', 'dark');
         }
     });
 });
